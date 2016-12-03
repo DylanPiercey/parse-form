5d7617dcd89ea241444bebdf105658e48eebc5c3
Dylan Piercey <pierceydylan@gmail.com>
2016-12-02 18:59:33 -0800

    Switch to standard style and misc cleanup

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-"use strict";
+'use strict'
 
-var qset      = require("q-set");
-var fset      = qset.flat;
-var temp      = [];
+var qset = require('q-set')
+var fset = qset.flat
 var validTags = {
-	INPUT: true,
-	TEXTAREA: true,
-	SELECT: true,
-	BUTTON: true
-};
+  INPUT: true,
+  TEXTAREA: true,
+  SELECT: true,
+  BUTTON: true
+}
 
 /*
  * Serialize a html form as JS object.
@@ -16,67 +15,67 @@ var validTags = {
  * @param {<Form/>} form
  * @returns { Array<Array> }
  */
-module.exports = function FormJSON (form, flat) {
-	if (!form || form.nodeName !== "FORM") {
-		throw new Error("Can only parse form elements.");
-	}
+module.exports = function parseForm (form, flat) {
+  if (!form || form.nodeName !== 'FORM') {
+    throw new Error('Can only parse form elements.')
+  }
 
-	var set         = flat ? fset : qset;
-	var isMultiPart = form.enctype === "multipart/form-data";
-	var elements    = form.elements;
-	var body        = {};
-	var files       = isMultiPart ? {} : undefined;
-	var element;
+  var set = flat ? fset : qset
+  var isMultiPart = form.enctype === 'multipart/form-data'
+  var elements = form.elements
+  var body = {}
+  var files = isMultiPart ? {} : undefined
+  var element
 
-	for (var i = 0, len = elements.length; i < len; i++) {
-		element = elements[i];
+  for (var i = 0, len = elements.length; i < len; i++) {
+    element = elements[i]
 
-		// Check if this element should be serialized.
-		if (element.disabled || !(element.name && validTags[element.nodeName])) {
-			continue;
-		}
+    // Check if this element should be serialized.
+    if (element.disabled || !(element.name && validTags[element.nodeName])) {
+      continue
+    }
 
-		var name    = element.name;
-		var options = element.options;
+    var name = element.name
+    var options = element.options
 
-		switch (element.type) {
-			case "submit":
-				// We check if the submit button is active
-				// otherwise all type=submit buttons would be serialized.
-				if (element === document.activeElement) set(body, name, element.value);
-				break;
-			case "checkbox":
-			case "radio":
-				if (element.checked) set(body, name, element.value);
-				break;
-			case "select-one":
-				if (element.selectedIndex >= 0) set(body, name, options[element.selectedIndex].value);
-				break;
-			case "select-multiple":
-				var selected = [];
-				var option;
-				for (var j = 0, _len = options.length; j < _len; j++) {
-					option = options[j];
-					if (option && option.selected) selected.push(option.value);
-				}
+    switch (element.type) {
+      case 'submit':
+        // We check if the submit button is active
+        // otherwise all type=submit buttons would be serialized.
+        if (element === document.activeElement) set(body, name, element.value)
+        break
+      case 'checkbox':
+      case 'radio':
+        if (element.checked) set(body, name, element.value)
+        break
+      case 'select-one':
+        if (element.selectedIndex >= 0) set(body, name, options[element.selectedIndex].value)
+        break
+      case 'select-multiple':
+        var selected = []
+        var option
+        for (var _a = 0, _lenA = options.length; _a < _lenA; _a++) {
+          option = options[_a]
+          if (option && option.selected) selected.push(option.value)
+        }
 
-				set(body, name, selected);
-				break;
-			case "file":
-				var fileList = element.files;
-				if (isMultiPart && fileList) {
-					for (var _i = 0, _len = fileList.length; _i < _len; _i++) {
-						set(files, name, fileList[_i]);
-					}
-				}
-				break;
-			default:
-				set(body, name, element.value);
-		}
-	}
+        set(body, name, selected)
+        break
+      case 'file':
+        var fileList = element.files
+        if (isMultiPart && fileList) {
+          for (var _b = 0, _lenB = fileList.length; _b < _lenB; _b++) {
+            set(files, name, fileList[_b])
+          }
+        }
+        break
+      default:
+        set(body, name, element.value)
+    }
+  }
 
-	return {
-		body: body,
-		files: files
-	};
-};
+  return {
+    body: body,
+    files: files
+  }
+}
