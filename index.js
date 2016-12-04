aa4f25cda437dc0da100c6bee7fd662345e732ed
Dylan Piercey <pierceydylan@gmail.com>
2016-12-03 19:14:09 -0800

    Patch submit button in safari

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var _clickTarget = null
 var qset = require('q-set')
 var fset = qset.flat
 var validTags = {
@@ -7,6 +8,25 @@ var validTags = {
   TEXTAREA: true,
   SELECT: true,
   BUTTON: true
+}
+
+/**
+ * Tracks which button submitted a form last.
+ */
+window.addEventListener('click', function patchActiveElement (e) {
+  var el = e.target
+  // Find an <button> element that may have been clicked.
+  while (el != null && (el.nodeName !== 'BUTTON' || el.type !== 'submit')) el = el.parentNode
+  // Store the button that was clicked.
+  _clickTarget = el
+})
+
+/**
+ * Patch for document.activeElement for safari.
+ */
+function getActiveElement () {
+  if (document.activeElement === document.body) return _clickTarget
+  return document.activeElement
 }
 
 /*
@@ -22,34 +42,33 @@ module.exports = function parseForm (form, flat) {
 
   var set = flat ? fset : qset
   var isMultiPart = form.enctype === 'multipart/form-data'
-  var elements = form.elements
+  var nodes = form.elements
   var body = {}
   var files = isMultiPart ? {} : undefined
-  var element
 
-  for (var i = 0, len = elements.length; i < len; i++) {
-    element = elements[i]
+  for (var i = 0, len = nodes.length; i < len; i++) {
+    var el = nodes[i]
 
-    // Check if this element should be serialized.
-    if (element.disabled || !(element.name && validTags[element.nodeName])) {
+    // Check if this el should be serialized.
+    if (el.disabled || !(el.name && validTags[el.nodeName])) {
       continue
     }
 
-    var name = element.name
-    var options = element.options
+    var name = el.name
+    var options = el.options
 
-    switch (element.type) {
+    switch (el.type) {
       case 'submit':
         // We check if the submit button is active
         // otherwise all type=submit buttons would be serialized.
-        if (element === document.activeElement) set(body, name, element.value)
+        if (el === getActiveElement()) set(body, name, el.value)
         break
       case 'checkbox':
       case 'radio':
-        if (element.checked) set(body, name, element.value)
+        if (el.checked) set(body, name, el.value)
         break
       case 'select-one':
-        if (element.selectedIndex >= 0) set(body, name, options[element.selectedIndex].value)
+        if (el.selectedIndex >= 0) set(body, name, options[el.selectedIndex].value)
         break
       case 'select-multiple':
         var selected = []
@@ -62,7 +81,7 @@ module.exports = function parseForm (form, flat) {
         set(body, name, selected)
         break
       case 'file':
-        var fileList = element.files
+        var fileList = el.files
         if (isMultiPart && fileList) {
           for (var _b = 0, _lenB = fileList.length; _b < _lenB; _b++) {
             set(files, name, fileList[_b])
@@ -70,7 +89,7 @@ module.exports = function parseForm (form, flat) {
         }
         break
       default:
-        set(body, name, element.value)
+        set(body, name, el.value)
     }
   }
 
